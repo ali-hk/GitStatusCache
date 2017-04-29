81d505f5888e9359394e214ff462e09fbc2170a6
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2017-04-29 12:13:11 -0700

    Add some basic watcher infrastructure

---
+++ src/GitStatusCache/IRepositoryWatcher.cs
@@ -0,0 +1,6 @@
+﻿namespace GitStatusCache
+{
+    public interface IRepositoryWatcher
+    {
+    }
+}
---
+++ src/GitStatusCache/RepositoryWatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GitStatusCache
+{
+    public class RepositoryWatcher : IRepositoryWatcher
+    {
+        private readonly FileSystemWatcher _watcher;
+
+        public RepositoryWatcher(string path)
+        {
+            _watcher = new FileSystemWatcher(path);
+            _watcher.IncludeSubdirectories = true;
+        }
+    }
+}
