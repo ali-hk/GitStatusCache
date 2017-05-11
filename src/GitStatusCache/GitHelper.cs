76f287f648b590c5eb9bbd224a2247fd87f8dc3e
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2017-05-11 01:09:05 -0700

    Add background status referesh

---
+++ src/GitStatusCache/GitHelper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GitStatusCache
+{
+    public static class GitHelper
+    {
+        private readonly static Regex statusRegex = new Regex("^(?<index>[^#])(?<working>.) (?<path1>.*?)(?: -> (?<path2>.*))?$", RegexOptions.Compiled);
+        private readonly static Regex branchRegex = new Regex(@"^## (?<branch>\S+?)(?:\.\.\.(?<upstream>\S+))?(?: \[(?:ahead (?<ahead>\d+))?(?:, )?(?:behind (?<behind>\d+))?(?<gone>gone)?\])?$", RegexOptions.Compiled);
+        private readonly static Regex initRegex = new Regex(@"^## Initial commit on (?<branch>\S+)$", RegexOptions.Compiled);
+
+        public static string GetWorkingPath(string repoPath)
+        {
+            return Path.GetDirectoryName(repoPath);
+        }
+
+        public static RepositoryStatus ParseStatusOutput(string output)
+        {
+            var lines = output.Split(new[] { "\n", "\r", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var indexAdded = new List<string>();
+            var indexModified = new List<string>();
+            var indexDeleted = new List<string>();
+            var indexConflicted = new List<string>();
+            var workingAdded = new List<string>();
+            var workingModified = new List<string>();
+            var workingDeleted = new List<string>();
+            var workingConflicted = new List<string>();
+            string branch = null;
+            string upstream = null;
+            int? aheadBy = null;
+            int? behindBy = null;
+            bool? upstreamGone = null;
+
+            foreach (var currLine in lines)
+            {
+                var statusResults = statusRegex.Match(currLine);
+                if (statusResults.Success)
+                {
+                    var indexStatus = statusResults.Groups["index"].Value;
+                    switch (indexStatus)
+                    {
+                        case "A":
+                            indexAdded.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "M":
+                        case "R":
+                        case "C":
+                            indexModified.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "D":
+                            indexDeleted.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "U":
+                            indexConflicted.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    var workingStatus = statusResults.Groups["working"].Value;
+                    switch (workingStatus)
+                    {
+                        case "?":
+                        case "A":
+                            workingAdded.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "M":
+                            workingModified.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "D":
+                            workingDeleted.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        case "U":
+                            workingConflicted.Add(statusResults.Groups["path1"].Value);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    continue;
+                }
+
+                var branchResults = branchRegex.Match(currLine);
+                if (branchResults.Success)
+                {
+                    branch = branchResults.Groups["branch"].Value;
+                    upstream = branchResults.Groups["upstream"].Value;
+                    var aheadGroup = branchResults.Groups["ahead"];
+                    if (aheadGroup.Success)
+                    {
+                        aheadBy = int.Parse(aheadGroup.Value);
+                    }
+
+                    var behindGroup = branchResults.Groups["behind"];
+                    if (behindGroup.Success)
+                    {
+                        behindBy = int.Parse(behindGroup.Value);
+                    }
+
+                    upstreamGone = branchResults.Groups["gone"].Value.Equals("gone", StringComparison.OrdinalIgnoreCase);
+                    continue;
+                }
+
+                var initResults = initRegex.Match(currLine);
+                if (initResults.Success)
+                {
+                    branch = branchResults.Groups["branch"].Value;
+                }
+            }
+
+            var newStatus = new RepositoryStatus
+            {
+                AheadBy = aheadBy ?? 0,
+                BehindBy = behindBy ?? 0,
+                Branch = branch,
+                IndexAdded = indexAdded,
+                IndexConflicted = indexConflicted,
+                IndexDeleted = indexDeleted,
+                IndexModified = indexModified,
+                WorkingAdded = workingAdded,
+                WorkingConflicted = workingConflicted,
+                WorkingDeleted = workingDeleted,
+                WorkingModified = workingModified,
+                Stashes = null,
+                Upstream = upstream,
+                UpstreamGone = upstreamGone ?? false
+            };
+
+            return newStatus;
+        }
+    }
+}
--- src/GitStatusCache/StatusCache.cs
+++ src/GitStatusCache/StatusCache.cs
@@ -4,24 +4,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace GitStatusCache
 {
     public class StatusCache
     {
+        private const int BACKGROUND_UPDATE_DELAY_MS = 500;
         private readonly ReaderWriterLockSlim _watcherLock = new ReaderWriterLockSlim();
         private readonly Dictionary<string, RepositoryWatcher> _watcherMap = new Dictionary<string, RepositoryWatcher>();
         private readonly ConcurrentDictionary<string, CachedStatus> _statusMap = new ConcurrentDictionary<string, CachedStatus>();
+        private readonly ConcurrentDictionary<string, ReaderWriterLockSlim> _statusLockMap = new ConcurrentDictionary<string, ReaderWriterLockSlim>();
 
         public bool TryGetStatus(string path, out RepositoryStatus status)
         {
             if (_statusMap.TryGetValue(path, out var cachedStatus) && cachedStatus.IsValid)
             {
+                Debug.WriteLine("Cache hit");
                 status = cachedStatus.Status;
                 return true;
             }
             else
             {
+                Debug.WriteLine("Cache miss");
                 WatchRepository(path);
                 status = null;
                 return false;
@@ -32,7 +37,7 @@ namespace GitStatusCache
         {
             if (_statusMap.TryGetValue(path, out var cachedStatus))
             {
-                 var newStatus = cachedStatus.Update(status);
+                var newStatus = cachedStatus.Update(status);
                 if (!_statusMap.TryUpdate(path, newStatus, cachedStatus))
                 {
                     Debug.WriteLine("Cached status wasn't the same as retrieved status, didn't update status.");
@@ -67,20 +72,46 @@ namespace GitStatusCache
 
         private void OnRepositoryChanged(object watcherObj, string changedPath)
         {
-            if(changedPath.EndsWith("index.lock", StringComparison.OrdinalIgnoreCase) || changedPath.EndsWith(".git", StringComparison.OrdinalIgnoreCase))
+            if (changedPath != null && (changedPath.EndsWith("index.lock", StringComparison.OrdinalIgnoreCase) || changedPath.EndsWith(".git", StringComparison.OrdinalIgnoreCase)))
             {
                 return;
             }
 
             var watcher = watcherObj as RepositoryWatcher;
             var repoPath = watcher.RepositoryPath;
-            if(_statusMap.TryGetValue(repoPath, out var status))
+            if (_statusMap.TryGetValue(repoPath, out var status))
             {
                 var newStatus = status.Invalidate();
-                if(!_statusMap.TryUpdate(repoPath, newStatus, status))
+                if (!_statusMap.TryUpdate(repoPath, newStatus, status))
                 {
                     Debug.WriteLine("Cached status wasn't the same as retrieved status, didn't update validity.");
                 }
+
+                Task.Delay(BACKGROUND_UPDATE_DELAY_MS).ContinueWith(t =>
+                {
+                    ReaderWriterLockSlim statusLock = null;
+                    if (!_statusLockMap.TryGetValue(repoPath, out statusLock))
+                    {
+                        statusLock = new ReaderWriterLockSlim();
+                        _statusLockMap.TryAdd(repoPath, statusLock);
+                    }
+
+                    using (new ScopedReaderWriterLock(statusLock, ReaderWriterLockType.Writing))
+                    {
+                        if (!_statusMap.TryGetValue(repoPath, out var cachedStatus) || !cachedStatus.IsValid)
+                        {
+                            Debug.WriteLine("Calling git status");
+                            var repoWorkingPath = GitHelper.GetWorkingPath(repoPath);
+                            var process = Process.Start(new ProcessStartInfo { FileName = "git", Arguments = "-c core.quotepath=false -c color.status=false -c status.relativePaths=false status --no-lock-index --short --branch", CreateNoWindow = true, UseShellExecute = false, WorkingDirectory = repoWorkingPath, RedirectStandardOutput = true });
+                            var output = process.StandardOutput.ReadToEnd();
+                            process.WaitForExit();
+
+                            Debug.WriteLine("git status completed");
+                            var updatedStatus = GitHelper.ParseStatusOutput(output);
+                            _statusMap.TryUpdate(repoPath, cachedStatus.Update(updatedStatus), cachedStatus);
+                        }
+                    }
+                });
             }
         }
     }
